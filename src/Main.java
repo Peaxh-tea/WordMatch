5ba66c435eb5829f6485b0f1d218dfe76b85e04f
AustinArvelo <austina46@nycstudents.net>
2025-10-14 13:18:02 -0400

    Added findBetterGuess method and scoreGuess method

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,23 @@
 public class Main {
+    public static void main(String [] args){
+        WordMatch w = new WordMatch("mississippi");
+        System.out.println(w.scoreGuess("i"));
+        System.out.println(w.scoreGuess("iss"));
+        System.out.println(w.scoreGuess("issipp"));
+        System.out.println(w.scoreGuess("mississippi"));
+        WordMatch a = new WordMatch("aaaabb");
+        System.out.println(a.scoreGuess("a"));
+        System.out.println(a.scoreGuess("aa"));
+        System.out.println(a.scoreGuess("aaa"));
+        System.out.println(a.scoreGuess("aabb"));
+        System.out.println(a.scoreGuess("c"));
+        WordMatch game = new WordMatch ("concatenation");
+        System.out.println(game.scoreGuess("ten"));
+        System.out.println(game.scoreGuess("nation"));
+        System.out.println(game.findBetterGuess("ten", "nation"));
+        System.out.println(game.scoreGuess("con"));
+        System.out.println(game.scoreGuess("cat"));
+        System.out.println(game.findBetterGuess("con", "cat"));
+
+    }
 }
--- src/WordMatch.java
+++ src/WordMatch.java
@@ -1,2 +1,23 @@
 public class WordMatch {
+    private String secret;
+    public WordMatch(String word){
+        secret = word;
+    }
+    public int scoreGuess(String guess){
+        int score = 0;
+        for (int i = 0; i <= secret.length() - guess.length(); i++){
+            if (secret.substring(i, i + guess.length()).equals(guess)){
+                score++;
+            }
+        }
+        return score * guess.length() * guess.length();
+    }
+    public String findBetterGuess(String guess1, String guess2){
+        int guessA = scoreGuess(guess1);
+        int guessB = scoreGuess(guess2);
+        if (guessB > guessA){
+            return guess2;
+        }
+        return guess1;
+    }
 }
